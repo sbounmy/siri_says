d59b480f7ce40bdfc643ec80a14253209d3ab709
Bounmy Stephane <stephanebounmy@gmail.com>
2011-10-27 03:14:34 -0700

    changed hash syntax to be compatible  under ruby 1.8.7

--- lib/siri_says.rb
+++ lib/siri_says.rb
@@ -14,8 +14,8 @@ module SiriSays
     class << self
       def add( regex, block )
         (@plugins ||= []) << {
-          regex: regex,
-          block: block
+          :regex => regex,
+          :block => block
         }
       end
       
@@ -56,9 +56,9 @@ module SiriSays
       def ranges
         [
           # Sceduled events
-          { start: scheduled_start, end: scheduled_end, all_day: false },
+          { :start => scheduled_start, :end => scheduled_end, all_day: false },
           # Immediate events are all-day events
-          { start: immediate_start, end: immediate_end, all_day: true }
+          { :start => immediate_start, :end => immediate_end, all_day: true }
         ]
       end
       
